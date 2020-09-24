8d502ef3bc33455369200a751ea6c2c34be03cae
ClaudianyMartins <41713981+ClaudianyMartins@users.noreply.github.com>
2020-09-24 15:23:04 -0300

    alteracoes nome imagens

--- src/home.js
+++ src/home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import Cerva from "./imagem/cerva.jpeg";
-import Imperio from "./imagem/imperio.png";
+import cerva from "./imagem/cerva.jpeg";
+import imperio from "./imagem/imperio.png";
 
 export default class Home extends Component {
   render() {
@@ -10,11 +10,11 @@ export default class Home extends Component {
       <div>
         <Carousel autoPlay>
           <div>
-            <img alt="Cerva" src={Cerva} />
+            <img alt="Cerva" src={cerva} />
             <p className="legend">Legend 1</p>
           </div>
           <div>
-            <img alt="Imperio" src={Imperio} />
+            <img alt="Imperio" src={imperio} />
             <p className="legend">Legend 2</p>
           </div>
         </Carousel>
